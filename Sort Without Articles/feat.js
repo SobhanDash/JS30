16a8e489daa6a2ea52e9bd026b2d351655df514b
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2021-05-04 16:21:14 +0530

    added features

--- Sort Without Articles/feat.js
+++ Sort Without Articles/feat.js
@@ -1,7 +1,5 @@
-const bands = [
+const movies = [
 	"The Prestige",
-	"The Edge of Tomorrow",
-	"Insidious",
 	"A Star is Born",
 	"The Conjuring",
 	"Batman v Superman",
@@ -13,3 +11,19 @@ const bands = [
 	"Fight Club",
 	"Avengers: Endgame",
 ]
+
+function reArrange(movieName) {
+    return movieName.replace(/^('a'|'the'|'an')/i, '').trim();
+}
+
+const sortMovies = movies.sort(function(a,b){
+    if(reArrange(a) > reArrange(b)) {
+        return 1;
+    }else{
+        return -1;
+    }
+})
+
+//console.log(sortMovies)
+
+document.querySelector('#movies').innerHTML = sortMovies.map(movie => `<li>${movie}</li>`).join('');
