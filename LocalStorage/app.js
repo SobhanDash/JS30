615c4d751335e9d1db52b2ddd2c4523a94c0dee0
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2021-05-06 21:31:49 +0530

    minor fix

--- LocalStorage/app.js
+++ LocalStorage/app.js
@@ -1,49 +1,49 @@
 const addItems = document.querySelector(".add-items")
-			const itemsList = document.querySelector(".plates")
-			const items = JSON.parse(localStorage.getItem("items")) || []
+const itemsList = document.querySelector(".plates")
+const items = JSON.parse(localStorage.getItem("items")) || []
 
-			function addItem(e) {
-				e.preventDefault()
-				//console.log('hey')
-				const text = (this.querySelector("[name=item]")).value
-				const item = {
-					//text: text
-					text,
-					done: false
-				}
-				items.push(item)
-				populateList(items, itemsList)
-				localStorage.setItem("items", JSON.stringify(items))
-				this.reset()
-				//console.log(item)
-			}
+function addItem(e) {
+	e.preventDefault()
+	//console.log('hey')
+	const text = this.querySelector("[name=item]").value
+	const item = {
+		//text: text
+		text,
+		done: false,
+	}
+	items.push(item)
+	populateList(items, itemsList)
+	localStorage.setItem("items", JSON.stringify(items))
+	this.reset()
+	//console.log(item)
+}
 
-			//did this so if in future we can pass a different array list and any destiantion html element
-			function populateList(dishes = [], dishList) {
-				dishList.innerHTML = dishes
-					.map((dish, i) => {
-						return `
+//did this so if in future we can pass a different array list and any destiantion html element
+function populateList(dishes = [], dishList) {
+	dishList.innerHTML = dishes
+		.map((dish, i) => {
+			return `
         <li>
           <input type="checkbox" data-index=${i} id='item${i}' ${
-							dish.done ? "checked" : ""
-						}" />
+				dish.done ? "checked" : ""
+			} />
           <label for="item${i}">${dish.text}</label>
         </li>
       `
-					}).join("")
-			}
+		})
+		.join("")
+}
 
-			function toggleBox(e) {
-        if (!e.target.matches('input')) return; // skip this unless it's an input
-        const el = e.target;
-        const index = el.dataset.index;
-        items[index].done = !items[index].done;
-        localStorage.setItem('items', JSON.stringify(items));
-        populateList(items, itemsList);
-      }
-      
+function toggleBox(e) {
+	if (!e.target.matches("input")) return // skip this unless it's an input
+	const el = e.target
+	const index = el.dataset.index
+	items[index].done = !items[index].done
+	localStorage.setItem("items", JSON.stringify(items))
+	populateList(items, itemsList)
+}
 
-			addItems.addEventListener("submit", addItem)
-			itemsList.addEventListener("click", toggleBox)
+addItems.addEventListener("submit", addItem)
+itemsList.addEventListener("click", toggleBox)
 
-			populateList(items, itemsList)
+populateList(items, itemsList)
